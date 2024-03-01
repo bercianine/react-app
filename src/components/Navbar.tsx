00b9c2124b1ac675aac5da7a2bd591773199792e
Sofia Bercianine <sfbercianine@gmail.com>
2024-02-29 22:33:36 -0300

    Changed it up to be a new app

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,27 @@
-import { HiMiniUser, HiBookmark } from "react-icons/hi2";
+import { HiMiniUser, HiMiniPlay } from "react-icons/hi2";
+import { MdColorLens } from "react-icons/md";
+import { HiCommandLine } from "react-icons/hi2";
 import NavbarItem from "./NavbarItem";
+import displayLogo from "../assets/display-logo.png";
+import lines from "../assets/lines.png";
 
 const Navbar = () => {
   const menu = [
-    { name:'My Lists', icon:HiBookmark },
+    { name:'All content', icon:HiMiniPlay },
+    { name:'Design related', icon:MdColorLens },
+    { name:'Development related', icon:HiCommandLine },
     { name:'Login', icon:HiMiniUser }
   ]
 
   return (
     <div className='navbar'>
-      <h1>🍿 Get Popin'</h1>
       <div className='navbar-menu'>
         {menu.map((item) => (
           <NavbarItem name={item.name} Icon={item.icon}/>
         ))}
       </div>
+      <img src={displayLogo} alt="Display" />
+      <img src={lines} alt="Gradient" />
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
-import './Navbar.css';
-import './App.css'
+import './stylesheets/App.css'
+import './stylesheets/Navbar.css';
+import './stylesheets/Banner.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
